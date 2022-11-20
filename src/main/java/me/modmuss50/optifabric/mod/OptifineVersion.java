50000839e37fd5698495f4dfc8fa316698221f4a
Martín R. O <contacto@isnakebuzz.com>
2022-11-20 06:20:29 -0300

    Using fileUtils

--- src/main/java/me/modmuss50/optifabric/mod/OptifineVersion.java
+++ src/main/java/me/modmuss50/optifabric/mod/OptifineVersion.java
@@ -1,7 +1,7 @@
 package me.modmuss50.optifabric.mod;
 
 import me.modmuss50.optifabric.patcher.ASMUtils;
-import net.fabricmc.loader.api.FabricLoader;
+import me.modmuss50.optifabric.util.FileUtils;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldNode;
 import org.zeroturnaround.zip.ZipUtil;
@@ -14,130 +14,130 @@ import java.util.jar.JarFile;
 
 public class OptifineVersion {
 
-	public static String version;
-	public static String minecraftVersion;
-	public static JarType jarType;
+    public static String version;
+    public static String minecraftVersion;
+    public static JarType jarType;
 
-	public static File findOptifineJar() throws IOException {
-		File modsDir = new File(FabricLoader.getInstance().getGameDirectory(), "mods");
-		File[] mods = modsDir.listFiles();
+    public static File findOptifineJar() throws IOException {
+        File modsDir = FileUtils.getModsFolder();
+        File[] mods = modsDir.listFiles();
 
-		File optifineJar = null;
+        File optifineJar = null;
 
-		if (mods != null) {
-			for (File file : mods) {
-				if (file.isDirectory()) {
-					continue;
-				}
-				if (file.getName().endsWith(".jar")) {
-					JarType type = getJarType(file);
-					if (type.error) {
-						if (!type.name().equals("INCOMPATIBE")) {
-							throw new RuntimeException("An error occurred when trying to find the optifine jar: " + type.name());
-						} else {
-							continue;
-						}
-					}
-					if (type == JarType.OPIFINE_MOD || type == JarType.OPTFINE_INSTALLER) {
-						if (optifineJar != null) {
-							OptifabricError.setError("Found 2 or more optifine jars, please ensure you only have 1 copy of optifine in the mods folder!");
-							throw new FileNotFoundException("Multiple optifine jars");
-						}
-						jarType = type;
-						optifineJar = file;
-					}
-				}
-			}
-		}
+        if (mods != null) {
+            for (File file : mods) {
+                if (file.isDirectory()) {
+                    continue;
+                }
+                if (file.getName().endsWith(".jar")) {
+                    JarType type = getJarType(file);
+                    if (type.error) {
+                        if (!type.name().equals("INCOMPATIBE")) {
+                            throw new RuntimeException("An error occurred when trying to find the optifine jar: " + type.name());
+                        } else {
+                            continue;
+                        }
+                    }
+                    if (type == JarType.OPIFINE_MOD || type == JarType.OPTFINE_INSTALLER) {
+                        if (optifineJar != null) {
+                            OptifabricError.setError("Found 2 or more optifine jars, please ensure you only have 1 copy of optifine in the mods folder!");
+                            throw new FileNotFoundException("Multiple optifine jars");
+                        }
+                        jarType = type;
+                        optifineJar = file;
+                    }
+                }
+            }
+        }
 
-		if (optifineJar != null) {
-			return optifineJar;
-		}
+        if (optifineJar != null) {
+            return optifineJar;
+        }
 
-		OptifabricError.setError("OptiFabric could not find the Optifine jar in the mods folder.");
-		throw new FileNotFoundException("Could not find optifine jar");
-	}
+        OptifabricError.setError("OptiFabric could not find the Optifine jar in the mods folder.");
+        throw new FileNotFoundException("Could not find optifine jar");
+    }
 
-	private static JarType getJarType(File file) throws IOException {
-		ClassNode classNode;
-		try (JarFile jarFile = new JarFile(file)) {
-			JarEntry jarEntry = jarFile.getJarEntry("Config.class"); // Pre 1.14.3 location
-			if (jarEntry == null) {
-				return JarType.SOMETHINGELSE;
-			}
-			classNode = ASMUtils.asClassNode(jarEntry, jarFile);
-		}
+    private static JarType getJarType(File file) throws IOException {
+        ClassNode classNode;
+        try (JarFile jarFile = new JarFile(file)) {
+            JarEntry jarEntry = jarFile.getJarEntry("Config.class"); // Pre 1.14.3 location
+            if (jarEntry == null) {
+                return JarType.SOMETHINGELSE;
+            }
+            classNode = ASMUtils.asClassNode(jarEntry, jarFile);
+        }
 
-		for (FieldNode fieldNode : classNode.fields) {
-			if (fieldNode.name.equals("VERSION")) {
-				version = (String) fieldNode.value;
-			}
-			if (fieldNode.name.equals("MC_VERSION")) {
-				minecraftVersion = (String) fieldNode.value;
-			}
-		}
+        for (FieldNode fieldNode : classNode.fields) {
+            if (fieldNode.name.equals("VERSION")) {
+                version = (String) fieldNode.value;
+            }
+            if (fieldNode.name.equals("MC_VERSION")) {
+                minecraftVersion = (String) fieldNode.value;
+            }
+        }
 
-		if (version == null || version.isEmpty() || minecraftVersion == null || minecraftVersion.isEmpty()) {
-			return JarType.INCOMPATIBE;
-		}
+        if (version == null || version.isEmpty() || minecraftVersion == null || minecraftVersion.isEmpty()) {
+            return JarType.INCOMPATIBE;
+        }
 
-		String currentMcVersion = "1.8.9";
+        String currentMcVersion = "1.8.9";
 
-		if (!currentMcVersion.equals(minecraftVersion)) {
-			OptifabricError.setError(String.format("This version of optifine is not compatible with the current minecraft version\n\n Optifine requires %s you have %s", minecraftVersion, currentMcVersion));
-			return JarType.INCOMPATIBE;
-		}
+        if (!currentMcVersion.equals(minecraftVersion)) {
+            OptifabricError.setError(String.format("This version of optifine is not compatible with the current minecraft version\n\n Optifine requires %s you have %s", minecraftVersion, currentMcVersion));
+            return JarType.INCOMPATIBE;
+        }
 
-		Holder<Boolean> isInstaller = new Holder<>(false);
-		ZipUtil.iterate(file, (in, zipEntry) -> {
-			if (zipEntry.getName().startsWith("patch/")) {
-				isInstaller.setValue(true);
-			}
-		});
+        Holder<Boolean> isInstaller = new Holder<>(false);
+        ZipUtil.iterate(file, (in, zipEntry) -> {
+            if (zipEntry.getName().startsWith("patch/")) {
+                isInstaller.setValue(true);
+            }
+        });
 
-		if (isInstaller.getValue()) {
-			return JarType.OPTFINE_INSTALLER;
-		} else {
-			return JarType.OPIFINE_MOD;
-		}
-	}
+        if (isInstaller.getValue()) {
+            return JarType.OPTFINE_INSTALLER;
+        } else {
+            return JarType.OPIFINE_MOD;
+        }
+    }
 
-	public enum JarType {
-		OPIFINE_MOD(false),
-		OPTFINE_INSTALLER(false),
-		INCOMPATIBE(true),
-		SOMETHINGELSE(false);
+    public enum JarType {
+        OPIFINE_MOD(false),
+        OPTFINE_INSTALLER(false),
+        INCOMPATIBE(true),
+        SOMETHINGELSE(false);
 
-		boolean error;
+        boolean error;
 
-		JarType(boolean error) {
-			this.error = error;
-		}
+        JarType(boolean error) {
+            this.error = error;
+        }
 
-		public boolean isError() {
-			return error;
-		}
-	}
+        public boolean isError() {
+            return error;
+        }
+    }
 
-	private static class Holder<T> {
+    private static class Holder<T> {
 
-		T value;
+        T value;
 
-		private Holder(T value) {
-			this.value = value;
-		}
+        private Holder(T value) {
+            this.value = value;
+        }
 
-		public T getValue() {
-			return value;
-		}
+        public static <T> Holder<T> of(T value) {
+            return new Holder<>(value);
+        }
 
-		public void setValue(T value) {
-			this.value = value;
-		}
+        public T getValue() {
+            return value;
+        }
 
-		public static <T> Holder<T> of(T value) {
-			return new Holder<>(value);
-		}
+        public void setValue(T value) {
+            this.value = value;
+        }
 
-	}
+    }
 }
